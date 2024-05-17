55f30f2d3b5fad7c1cf466ad41553ec44fc47036
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-17 14:17:40 +0900

    init commit

---
+++ src/main/java/com/example/schedulemanagementserver/ScheduleManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/schedulemanagementserver/model/Schedule.java
@@ -0,0 +1,4 @@
+package com.example.schedulemanagementserver.model;
+
+public class Schedule {
+}
