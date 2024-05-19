e692e20fe4e6305f6401e341e301a6a7422fdf62
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 05:05:53 +0900

    Feat: Enable JPA repositories
    - Added @EnableJpaRepositories annotation to TodoManagementApplication
    - This allows Spring Data JPA repositories to be used in the application

--- src/main/java/io/sillysillyman/todomanagementapp/TodoManagementApplication.java
+++ src/main/java/io/sillysillyman/todomanagementapp/TodoManagementApplication.java
@@ -2,7 +2,9 @@ package io.sillysillyman.todomanagementapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class TodoManagementApplication {
 
