0202034dbfdba6e2c3d97f55b22a4d70de962758
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 02:19:16 +0900

    rename

---
+++ src/test/java/io/sillysillyman/todomanagementapp/TodoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sillysillyman.todomanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
