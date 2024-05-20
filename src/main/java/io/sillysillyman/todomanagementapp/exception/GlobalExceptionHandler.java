abf25979a435507f0b1b96dedc93c87a5ee81b4f
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 16:59:51 +0900

    feat: Add GlobalExceptionHandler for centralized exception handling
    - GlobalExceptionHandler 클래스 추가: 전역 예외 처리
    - handleTodoNotFoundException 메서드 추가: TodoNotFoundException 예외 처리
    - handlePasswordMismatchException 메서드 추가: PasswordMismatchException 예외 처리

---
+++ src/main/java/io/sillysillyman/todomanagementapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package io.sillysillyman.todomanagementapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TodoNotFoundException.class)
+    public ResponseEntity<String> handleTodoNotFoundException(TodoNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<String> handlePasswordMismatchException(PasswordMismatchException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+    }
+}
