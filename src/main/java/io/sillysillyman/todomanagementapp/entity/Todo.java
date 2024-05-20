5c750a077c31c6f237467819c492bf02895225c2
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 12:42:56 +0900

    feat: Add Lombok annotations and builder to Todo entity
    - @Builder 및 @NoArgsConstructor 애노테이션 추가
    - Todo 클래스에 빌더 패턴 생성자 구현

--- src/main/java/io/sillysillyman/todomanagementapp/entity/Todo.java
+++ src/main/java/io/sillysillyman/todomanagementapp/entity/Todo.java
@@ -1,4 +1,4 @@
-package io.sillysillyman.todomanagementapp.repository;
+package io.sillysillyman.todomanagementapp.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -6,10 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class Todo {
 
     @Id
@@ -24,6 +27,15 @@ public class Todo {
     private String userName;
 
     private String password;
-    
+
     private LocalDateTime createdAt;
+
+    @Builder
+    public Todo(String title, String content, String userName, String password) {
+        this.title = title;
+        this.content = content;
+        this.userName = userName;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
 }
