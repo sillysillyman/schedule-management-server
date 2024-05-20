11f56deceebf95ad121ea0abd8b2db3ff8f4e878
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 14:08:08 +0900

    feat: Add getTodo method to TodoManagementController
    - getTodo 메서드 추가: ID로 Todo 항목 조회
    - 조회된 Todo를 TodoManagementResponseDto로 변환하여 반환

--- src/main/java/io/sillysillyman/todomanagementapp/controller/TodoManagementController.java
+++ src/main/java/io/sillysillyman/todomanagementapp/controller/TodoManagementController.java
@@ -6,6 +6,8 @@ import io.sillysillyman.todomanagementapp.entity.Todo;
 import io.sillysillyman.todomanagementapp.service.TodoManagementService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,13 @@ public class TodoManagementController {
     public ResponseEntity<TodoManagementResponseDto> postTodo(
         @RequestBody TodoManagementRequestDto dto) {
         Todo todo = todoManagementService.createTodo(dto);
+        TodoManagementResponseDto responseDto = new TodoManagementResponseDto(todo);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @GetMapping("/v1.0/todo/{todoId}")
+    public ResponseEntity<TodoManagementResponseDto> getTodo(@PathVariable Long todoId) {
+        Todo todo = todoManagementService.getTodo(todoId);
         TodoManagementResponseDto responseDto = new TodoManagementResponseDto(todo);
         return ResponseEntity.ok().body(responseDto);
     }
