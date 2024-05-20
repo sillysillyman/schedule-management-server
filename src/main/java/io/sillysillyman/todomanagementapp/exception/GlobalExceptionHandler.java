a2673996232058fcf5cf7b5c2d1c5c612f1d4ce8
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 17:56:12 +0900

    feat: Add exception handler for validation errors
    - MethodArgumentNotValidException을 처리하는 핸들러 추가
    - 유효성 검사 실패 시 필드와 메시지를 반환하도록 수정

--- src/main/java/io/sillysillyman/todomanagementapp/exception/GlobalExceptionHandler.java
+++ src/main/java/io/sillysillyman/todomanagementapp/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package io.sillysillyman.todomanagementapp.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +20,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PasswordMismatchException.class)
     public ResponseEntity<String> handlePasswordMismatchException(PasswordMismatchException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+        MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
