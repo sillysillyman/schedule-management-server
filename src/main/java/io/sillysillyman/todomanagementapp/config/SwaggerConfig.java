7b410706aa5bc359483ae41a50da284dd9334559
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 17:36:03 +0900

    feat: Add SwaggerConfig for API documentation
    - SwaggerConfig 클래스 추가: API 문서화를 위한 Swagger 설정

---
+++ src/main/java/io/sillysillyman/todomanagementapp/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package io.sillysillyman.todomanagementapp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Todo Management API").version("1.0")
+            .description("API documentation for Todo Management Application"));
+    }
+
+}
