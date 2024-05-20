e936dbcdd09c8e522dc10eddede9ebc49f97196c
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 18:07:46 +0900

    feat: Add FileUploadController for handling file uploads
    - 파일 업로드 처리를 위한 FileUploadController 클래스 추가
    - /v1.0/files 매핑

---
+++ src/main/java/io/sillysillyman/todomanagementapp/controller/FileUploadController.java
@@ -0,0 +1,10 @@
+package io.sillysillyman.todomanagementapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1.0/files")
+public class FileUploadController {
+
+}
