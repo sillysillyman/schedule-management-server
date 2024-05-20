41cf0e59b34d716a415e3ed862c4cb62aed5c83e
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 13:00:31 +0900

    feat: Add TodoManagementResponseDto class
    - title, content, userName, createdAt 필드 추가
    - Todo 엔티티를 기반으로 하는 생성자 추가
    - 보안상의 이유로 password 필드 제외

---
+++ src/main/java/io/sillysillyman/todomanagementapp/dto/TodoManagementResponseDto.java
@@ -0,0 +1,23 @@
+package io.sillysillyman.todomanagementapp.dto;
+
+import io.sillysillyman.todomanagementapp.entity.Todo;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoManagementResponseDto {
+
+    private String title;
+    private String content;
+    private String userName;
+    private LocalDateTime createdAt;
+
+    public TodoManagementResponseDto(Todo todo) {
+        this.title = todo.getTitle();
+        this.content = todo.getContent();
+        this.userName = todo.getUserName();
+        this.createdAt = todo.getCreatedAt();
+    }
+}
