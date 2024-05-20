18fb5ffdd526abed10b9ea78d78bd80c63b1e48c
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 12:50:38 +0900

    refactor: Rename TodoRepository to TodoManagementRepository
    - 클래스 이름을 TodoRepository에서 TodoManagementRepository로 변경

--- src/main/java/io/sillysillyman/todomanagementapp/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package io.sillysillyman.todomanagementapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
