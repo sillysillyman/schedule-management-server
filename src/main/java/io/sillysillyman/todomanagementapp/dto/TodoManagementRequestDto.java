53544285801b2710395e565a225a360516f15664
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 17:54:04 +0900

    feat: Add validation annotations to TodoManagementRequestDto
    - 필수 필드에 @NotBlank 어노테이션 추가
    - 유효성 검사 실패 시 메시지 추가

--- src/main/java/io/sillysillyman/todomanagementapp/dto/TodoManagementRequestDto.java
+++ src/main/java/io/sillysillyman/todomanagementapp/dto/TodoManagementRequestDto.java
@@ -1,6 +1,7 @@
 package io.sillysillyman.todomanagementapp.dto;
 
 import io.sillysillyman.todomanagementapp.entity.Todo;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,13 @@ import lombok.Setter;
 @Setter
 public class TodoManagementRequestDto {
 
+    @NotBlank(message = "Title is mandatory")
     private String title;
+    @NotBlank(message = "Content is mandatory")
     private String content;
+    @NotBlank(message = "UserName is mandatory")
     private String userName;
+    @NotBlank(message = "Password is mandatory")
     private String password;
 
     public Todo toEntity() {
