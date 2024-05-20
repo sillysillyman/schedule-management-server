45a185adc141a852f454c1f2f61347bad2f45ff8
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-20 12:45:29 +0900

    feat: Enhance TodoManagementController with service and DTO
    - TodoManagementController에 TodoService 의존성 추가
    - 요청 본문을 처리하기 위해 TodoManagementRequestDto 도입
    - postTodo 메서드를 서비스와 DTO를 사용하도록 업데이트

--- src/main/java/io/sillysillyman/todomanagementapp/controller/TodoManagementController.java
+++ src/main/java/io/sillysillyman/todomanagementapp/controller/TodoManagementController.java
@@ -1,14 +1,22 @@
 package io.sillysillyman.todomanagementapp.controller;
 
+import io.sillysillyman.todomanagementapp.dto.TodoManagementRequestDto;
+import io.sillysillyman.todomanagementapp.service.TodoManagementService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class TodoManagementController {
 
+    public final TodoManagementService todoManagementService;
+
     @PostMapping("/v1.0/todo")
-    public ResponseEntity postTodo() {
+    public ResponseEntity postTodo(@RequestBody TodoManagementRequestDto dto) {
+        todoManagementService.createTodo(dto);
         return ResponseEntity.ok().build();
     }
 }
