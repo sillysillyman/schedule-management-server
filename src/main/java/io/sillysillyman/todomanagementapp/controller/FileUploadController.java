653a8293ed0fdc71cc4cb1d727da40ff8324eb29
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-31 11:57:58 +0900

    feat: Enhance file upload functionality and add download endpoint
    - 파일 업로드 시 콘텐츠 유형 검증 추가, JPG, JPEG, PNG 파일만 허용
    - 파일 이름으로 파일을 다운로드할 수 있는 새로운 엔드포인트 구현
    - 지원되지 않는 미디어 유형 및 파일을 찾을 수 없는 경우에 대한 에러 처리 추가

--- src/main/java/io/sillysillyman/todomanagementapp/controller/FileUploadController.java
+++ src/main/java/io/sillysillyman/todomanagementapp/controller/FileUploadController.java
@@ -5,14 +5,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Objects;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @RestController
 @RequestMapping("/v1.0/files")
@@ -25,6 +29,14 @@ public class FileUploadController {
         String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
         String uploadFilePath = UPLOAD_DIR + fileName;
 
+        String contentType = file.getContentType();
+        if (contentType == null ||
+            ((!contentType.equals("image/jpeg") && !contentType.equals("image/png")
+                && !contentType.equals("image/jpg")))) {
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body("Only JPG, JPEG, PNG files are allowed");
+        }
+
         try {
             Files.createDirectories(Paths.get(UPLOAD_DIR));
             file.transferTo(new File(uploadFilePath));
@@ -32,6 +44,18 @@ public class FileUploadController {
         } catch (IOException e) {
             return new ResponseEntity<>("Could not upload file: " + fileName,
                 HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("download/{fileName}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) {
+        try {
+            byte[] fileBytes = Files.readAllBytes(Paths.get(UPLOAD_DIR + fileName));
+            return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename\"" + fileName + "\"")
+                .body(fileBytes);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
