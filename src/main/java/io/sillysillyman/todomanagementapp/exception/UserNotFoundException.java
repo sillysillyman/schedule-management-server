d7f81ca70f7444d225fe0df02feaddaee56d6446
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-04 02:58:37 +0900

    feat: UserNotFoundException 클래스 추가
    주요 변경 사항:
    - 사용자 조회 실패 시 발생하는 UserNotFoundException 클래스 추가
    - RuntimeException을 상속하고, 메시지를 받는 생성자 구현

---
+++ src/main/java/io/sillysillyman/todomanagementapp/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package io.sillysillyman.todomanagementapp.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
